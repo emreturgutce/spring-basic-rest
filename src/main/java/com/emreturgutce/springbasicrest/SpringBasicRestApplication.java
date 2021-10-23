3d488d2366ecb6ac306d8f9007e6b312a62765dd
Emre Turgut <emreturgutcse@hotmail.com>
2021-10-23 13:05:33 +0300

    initial commit

---
+++ src/main/java/com/emreturgutce/springbasicrest/SpringBasicRestApplication.java
@@ -0,0 +1,13 @@
+package com.emreturgutce.springbasicrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBasicRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBasicRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/emreturgutce/springbasicrest/SpringBasicRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emreturgutce.springbasicrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBasicRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
